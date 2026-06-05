9745963911d6460a52aa2eb2cbfa898f3535e440
Mirza <113027159+M1rzoni@users.noreply.github.com>
2026-06-05 11:02:16 +0200

    okeyCreteNewTicket

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
   static const urlLocal = "http://172.23.207.83:5000";
-
   static const loginUrl = "$urlLocal/api/Auth/activate-user";
   static const categoriesUrl = "$urlLocal/api/Categories";
 }
